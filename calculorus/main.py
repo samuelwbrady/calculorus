0369aff878a4f5255150c3474b67291cc5e69a57
samuel.w.brady <samuelwarrenbrady@gmail.com>
2025-06-21 08:15:55 -0500

    commit module load tests

---
+++ calculorus/main.py
@@ -0,0 +1,26 @@
+
+#############
+# option #1 #
+#############
+#from math2.integration import *
+
+#print(add_numbers(5, 6))
+
+
+
+#############
+# option #2 #
+#############
+#from math2 import integration
+
+#print(integration.add_numbers(5, 6))
+
+
+#############
+# option #3 #
+#############
+# requires an __init__.py file that lists all the python files in the folder/module
+from math2 import *
+
+print(integration.add_numbers(5, 6))
+
